086025432e0486f141a337fd15addeca3ad64fd2
plavchan <plavchan@gmail.com>
2023-11-04 15:03:34 -0400

    test

--- readandplotcsv.py
+++ readandplotcsv.py
@@ -4,4 +4,8 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('aumicwasp.csv')
 x=df['JD']
 y=df['Flux']
-plt.plot(x,y)
+plt.plot(x,y,linestyle = "", marker = ".")
+plt.xlim(1280,1300)
+plt.xlabel("Julian Date (days)")
+plt.ylabel("Flux (Normalized)")
+plt.savefig('test.png')
